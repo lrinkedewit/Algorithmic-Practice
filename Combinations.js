6cd130367715693fe86d2c621d9873bd0eb78b0b
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-05-12 17:19:55 -0400

    latest algos

--- Combinations.js
+++ Combinations.js
@@ -18,36 +18,16 @@ Output:
 var combine = function(n, k) {
     // declare "output" set to an empty array
     const output = [];
-    // declare a counter set to n
-    let counter = 1;
-    // declare an array "numbers" to store all numbers from 1 to n
-    const numbers = [];
-    // declare a function "numberSet" to be called recursively until counter === n
-    function numberSet() {
-        // base case, counter === n
-        if (counter > n) return;
-        // push counter to "numbers" 
-        numbers.push(counter);
-        // counter += 1
-        counter += 1;
-        // recursively call "numberSet"
-        numberSet();
-    }
-    
-    // call "numberSet"
-    numberSet() 
-    numbers //? 
 
-    // declare a function "generate" to be called recursively that has a default parameter set to an empty array (tail call)
-    function generate (combo = [], first = 0) {
+    function generate (combo = [], first = 1) {
         // base case
         if (combo.length === k) {
             // push the copied array to our output
             return output.push([...combo]);
 
         }
-        for (let i = first; i < numbers.length; i += 1) {
-            combo.push(numbers[i])
+        for (let i = first; i <= n ; i += 1) {
+            combo.push(i)
             generate(combo, i + 1)
             combo.pop()
         }
@@ -58,7 +38,6 @@ var combine = function(n, k) {
 
     // return "output"
     return output;
-
 };
 
 combine(4, 2) //?
