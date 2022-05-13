436af2d63a787866848eb5296e280d489f645921
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-05-13 09:02:48 -0400

    Find all numbers disappeared in an array - solution

--- Find All Numbers Disappeared in an Array.js
+++ Find All Numbers Disappeared in an Array.js
@@ -20,21 +20,22 @@ var findDisappearedNumbers = function(nums) {
     
 
     // declare a function to be called recursively
-    const recurse = (counter = 0) => {
+    const recurse = (counter = 1) => {
         // base case = counter === nums.length
-        if (counter === nums.length) return;
+        if (counter > nums.length) return;
+        counter //? 
         // if counter does not exist in set, push it to output array
-
+        if (!set.has(counter)) output.push(counter)
+        recurse(counter += 1)
     };
 
     // call function
     recurse();
 
     // return output
-    return output;
+    return output; //?
 
 };
 
-const set1 = new Set([1,2,2,2,3,4,5]) //? 
-
 findDisappearedNumbers([4,3,2,7,8,2,3,1])
+findDisappearedNumbers([1,1]) //?
