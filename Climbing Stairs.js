d736a04723abc92589c9d2d924ce0a64a29467de
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-05-19 10:15:02 -0400

    brute force attempt at climbStairs

--- Climbing Stairs.js
+++ Climbing Stairs.js
@@ -3,6 +3,24 @@
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 
- var climbStairs = function(n) {
+// var climbStairs = function(n) {
+//     if (n === 1) return 1;
+//     let dp = n + 1;
+//     dp
+    
+// };
+
+var climbStairs = function(n) {
+
+    return helper(0,n);
     
 };
+
+function helper(i, n) {
+    if (i > n) return 0;
+    if (i === n) return 1;
+    return helper(i + 1, n) + helper(i + 2, n);
+}
+
+
+climbStairs
